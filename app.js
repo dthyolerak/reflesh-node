8d0d5be35c4a1ba396759e2fa8c33713c3dc6531
Donnex Thyolera Kamsonga <thyolerak@gmail.com>
2022-06-17 22:17:30 +0200

    displaying Welcome to task manager on port 3000 on the console as in browser for testing purpose

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Task Manager App')
+const express = require('express');
+//importing express server
+const exoress =require('express');
+const app = express();
+//hardcoding port number
+const port = 3000;
+//listen to port
+app.listen(port , console.log(`Welcome to task manager on port ${port}`));
+
+//router
+app.get('/task-manager', (req, res) =>{
+    res.send("Welcome to task manager app");
+})
