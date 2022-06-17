7fe9321a8e06fd74d8f1f28b03ee93776209f679
Donnex Thyolera Kamsonga <thyolerak@gmail.com>
2022-06-17 23:34:45 +0200

    done setting up basic structure of our api and tested in post man

--- controller/tasks.js
+++ controller/tasks.js
@@ -1,7 +1,29 @@
-const all_tasks = (req, res)=>{
+const { json } = require("express");
+
+const get_all_tasks = (req, res)=>{
     res.send("Hello from task");
 }
 
+
+const get_single_item = (req, res) =>{
+    res.send("hello from single item")
+}
+
+const add_new_item = (req, res)=>{
+    res.json(req.body);
+}
+
+const edit_item = (req,res)=>{
+    res.send("hello from edit item")
+}
+
+const delete_item = (req, res) =>{
+    res.send("hello from delete method")
+}
 module.exports = {
-    all_tasks,
+    get_all_tasks,
+    get_single_item,
+    add_new_item,
+    edit_item,
+    delete_item,
 }
--- router/tasks.js
+++ router/tasks.js
@@ -1,6 +1,14 @@
 //importing express
 const express = require('express');
 const router = express.Router();
-const {all_tasks} = require('../controller/tasks')
-router.route("/").get(all_tasks)
+
+//importing functions in controller tasks.js file
+const {get_all_tasks, 
+        delete_item,
+        get_single_item,
+        add_new_item,
+        edit_item,} = require('../controller/tasks');
+
+router.route("/").get(get_all_tasks).post(add_new_item);
+router.route("/:id").get(get_single_item).patch(edit_item).delete(delete_item);
 module.exports = router
